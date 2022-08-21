87cb55e475597f5b681b36c9972b8d052a7adac1
YuzuRyo61 <yuzuryo61@yuzulia.work>
2022-08-21 13:45:01 +0900

    Embed license

--- src/app/pages/about/children/license/license.component.ts
+++ src/app/pages/about/children/license/license.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import txt from '!!raw-loader!../../../../../../LICENSE';
+import packageJson from '../../../../../../package.json';
+import { getName } from '@tauri-apps/api/app';
 
 @Component({
   selector: 'app-license',
   templateUrl: './license.component.html',
   styleUrls: ['./license.component.scss']
 })
-export class LicenseComponent {
+export class LicenseComponent implements OnInit {
+  licenseText: string = txt;
+  name: string = packageJson.name;
+
   constructor() { }
 
+  async ngOnInit() {
+    this.name = await getName();
+  }
 }
